f6bb07547c147d9ba6a21fe9db4b2836e43b4fbc
John Clarke <johnclarke1996@gmail.com>
2021-03-21 13:29:55 -0400

    added fields to token model

--- models/TokenModel.js
+++ models/TokenModel.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 var TokenSchema = new mongoose.Schema({
 	name: {type: String, required: true},
 	address: {type: String, required: true},
+	decimal: {type: Number, required: true}
 }, {timestamps: true});
 
 module.exports = mongoose.model("Token", TokenSchema);
